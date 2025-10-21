94c65f746c895878ccb086442a020b90cc086232
hellmaujadidev <keelheel9@gmail.com>
2025-10-21 18:23:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "https://galaxy.pablocloud.biz.id", // Ubah jadi Domain panel Mu !!!
+  port: "5000" // Ubah Jadi Port Panel Mu !!!
+};
